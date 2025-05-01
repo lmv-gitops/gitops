fd4854bf5f9c51aa6a8a6072c624b4f8b603765a
lorena.miralles <lorena053@protonmail.com>
2025-05-01 18:35:37 +0200

    tf: create repo

---
+++ tf/variables.tf
@@ -0,0 +1,12 @@
+variable "utxgo_repository_administration_app_id" {
+  type = string
+}
+
+variable "utxgo_repository_administration_client_id" {
+  type = string
+}
+
+variable "utxgo_repository_administration_private_key" {
+  type = string
+  sensitive = true
+}
